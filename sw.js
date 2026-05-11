2a2dc944d53efbfade8b4e3fb8313550f61631b4
vaiswanara <71589931+vaiswanara@users.noreply.github.com>
2026-05-11 13:09:02 +0530

    v5

--- sw.js
+++ sw.js
@@ -1,41 +1,43 @@
-const CACHE_NAME = 'prashna-chakra-v10.1.5';
+const CACHE_NAME = "prashna-chakra-v10.1.11";
 const urlsToCache = [
-    './',
-    './index.html',
-    './chakra.png',
-    './data.json',
-    './icons/icon-192.png',
-    './icons/icon-512.png',
-    './manifest.json',
-    './Questions.json',
+  "./",
+  "./index.html",
+  "./chakra.png",
+  "./questions_info.json",
+  "./answers_info.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./manifest.json",
 ];
 
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
-            .then(() => self.skipWaiting())
-    );
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting()),
+  );
 });
 
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheName !== CACHE_NAME) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        }),
+      );
+    }),
+  );
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request, { ignoreSearch: true })
-            .then(response => response || fetch(event.request))
-            .catch(() => caches.match(event.request))
-    );
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request, { ignoreSearch: true })
+      .then((response) => response || fetch(event.request))
+      .catch(() => caches.match(event.request)),
+  );
 });
